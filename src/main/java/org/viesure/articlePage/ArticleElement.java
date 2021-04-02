14e4a49f52600d1f2ba14795aae421245862ecbf
János Csizmadia <>
2021-04-02 20:03:56 +0200

    Created class for ArticleElement

---
+++ src/main/java/org/viesure/articlePage/ArticleElement.java
@@ -0,0 +1,44 @@
+package org.viesure.articlePage;
+
+import org.openqa.selenium.WebElement;
+
+public class ArticleElement {
+    String title;
+    String author;
+    String release_date;
+
+    WebElement element;
+
+    public ArticleElement(WebElement element){
+        this.element = element;
+
+        initProperties();
+    }
+
+    public void initProperties(){
+        String elementText= element.getText();
+        String[] split = elementText.split(",");
+        title = split[0].trim();
+        author = split[1].replace("author:","").trim();
+        release_date = split[2].trim();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public WebElement getElement() {
+        return element;
+    }
+
+    @Override
+    public String toString(){
+        return "Title:\t" + this.title +
+                "\nAuthor:\t" + this.author +
+                "\nRelease Date:\t" + this.release_date;
+    }
+}
