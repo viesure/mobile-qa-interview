7df59348557d6bb7f9e559d602f9c10a65649cfc
János Csizmadia <>
2021-04-02 20:05:28 +0200

    Added wait till some content loads, added a method to get currently visible articles

--- src/main/java/org/viesure/articlePage/ArticleListPage.java
+++ src/main/java/org/viesure/articlePage/ArticleListPage.java
@@ -1,21 +1,47 @@
 package org.viesure.articlePage;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.viesure.base.BasePage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ArticleListPage extends BasePage {
 
-    ArticleListPageLocators locators;
+    private ArticleListPageLocators locators;
+    private List<ArticleElement> visibleArticleList;
 
     public ArticleListPage(AppiumDriver<?> driver){
         super(driver);
 
         locators = new ArticleListPageLocators(driver);
+
+        waitUntilPageIsPopulated();
+        getVisibleArticles();
     }
 
-    public int getListSize(){
-        return locators.articles.size();
+    public List<ArticleElement> getVisibleArticles(){
+        generateVisibleArticleList();
+        return visibleArticleList;
     }
 
+    private void waitUntilPageIsPopulated(){
+        WebDriverWait wait = new WebDriverWait(this.driver, 10);
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//android.view.ViewGroup/android.view.View/android.view.View"),1));
+    }
+
+    private void generateVisibleArticleList(){
+        //refreshing because if we scroll away, locators might changed
+        visibleArticleList = new ArrayList<>();
+        for (WebElement article: locators.articles){
+            if (!article.getText().contains("Dummy articles")){
+                this.visibleArticleList.add(new ArticleElement(article));
+            }
+        }
+    }
 }
