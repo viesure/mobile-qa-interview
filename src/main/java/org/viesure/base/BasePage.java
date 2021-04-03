e512877478bcf5ec6a73c9cc2cda4c073d6383ee
János Csizmadia <>
2021-04-03 10:36:47 +0200

    added isElementPresent helper method in BasePage

--- src/main/java/org/viesure/base/BasePage.java
+++ src/main/java/org/viesure/base/BasePage.java
@@ -2,6 +2,9 @@ package org.viesure.base;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
@@ -9,5 +12,21 @@ public class BasePage {
 
     public BasePage(AndroidDriver<AndroidElement> driver){
         this.driver = driver;
+    }
+
+    /**
+     * Returns if an element is present within given timeframe
+     * @param by the locator of the element
+     * @param timeoutInSeconds the maximum waiting time
+     * @return true if the element is visible
+     */
+    public boolean isElementPresent(By by, int timeoutInSeconds){
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+            return true;
+        }catch(Exception e){
+            return false;
+        }
     }
 }
