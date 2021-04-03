97e1484ea772441761eeb6dbbfe036d75c2d3e29
János Csizmadia <>
2021-04-03 12:22:22 +0200

    Created Article class

---
+++ src/main/java/org/viesure/page/common/Article.java
@@ -0,0 +1,35 @@
+package org.viesure.page.common;
+
+public class Article {
+
+    private int id;
+    private String title;
+    private String description;
+    private String author;
+    private String release_date;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
