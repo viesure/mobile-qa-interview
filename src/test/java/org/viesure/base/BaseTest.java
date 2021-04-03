6e78fec0e23c6667d1cf1a672049b35a2e32faef
János Csizmadia <>
2021-04-03 11:41:38 +0200

    made AndroidDriver public

--- src/test/java/org/viesure/base/BaseTest.java
+++ src/test/java/org/viesure/base/BaseTest.java
@@ -19,7 +19,7 @@ import java.net.URL;
 @Listeners(TestListener.class)
 public class BaseTest {
 
-    protected static AndroidDriver<AndroidElement> driver;
+    public AndroidDriver<AndroidElement> driver;
     private static AppiumDriverLocalService service;
 
     File app = new File(System.getProperty("user.dir") + "/app/android/" + "qa-interview.apk");
