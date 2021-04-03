c06c41f543f0f9d055c716150efdc8ed0e593bc0
János Csizmadia <>
2021-04-03 11:40:40 +0200

    added some system logging for test lifecycles, Failed tests will save screenshot to Allure

--- src/test/java/org/viesure/Listeners/TestListener.java
+++ src/test/java/org/viesure/Listeners/TestListener.java
@@ -1,48 +1,70 @@
 package org.viesure.Listeners;
 
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.viesure.base.BaseTest;
 
 public class TestListener implements ITestListener {
 
+    private static String getTestMethodName(ITestResult iTestResult) {
+        return iTestResult.getMethod().getConstructorOrMethod().getName();
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshotPNG (WebDriver driver) {
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog (String message) {
+        return message;
+    }
+
+
     @Override
     public void onTestStart(ITestResult result) {
-
+        System.out.println("onTestStart method for" +  getTestMethodName(result));
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-
+        System.out.println("onTestSuccess method for " +  getTestMethodName(result));
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+        System.out.println("onTestFailure method for " +  getTestMethodName(result));
 
+        Object testClass = result.getInstance();
+        WebDriver driver = ((BaseTest) testClass).driver;
+
+        //Save screenshot to allure
+        if (driver != null) {
+            System.out.println("Screenshot captured for test case:" + getTestMethodName(result));
+            saveScreenshotPNG(driver);
+        }
+
+        //Save a log on allure.
+        saveTextLog(getTestMethodName(result) + " failed and screenshot taken!");
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-
-    }
-
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-
-    }
-
-    @Override
-    public void onTestFailedWithTimeout(ITestResult result) {
-
+        System.out.println("onTestSkipped method for "+  getTestMethodName(result));
     }
 
     @Override
     public void onStart(ITestContext context) {
-
+        System.out.println("onStart method for " + context.getName());
     }
 
     @Override
     public void onFinish(ITestContext context) {
-
+        System.out.println("onFinish method for " + context.getName());
     }
 }
