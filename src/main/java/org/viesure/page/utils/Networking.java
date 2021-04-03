934fcf9158d0440bd7c1b6650a8db4c64736c5fd
János Csizmadia <>
2021-04-03 12:25:17 +0200

    Created Networking class, that loads the dummy data to verify against

---
+++ src/main/java/org/viesure/page/utils/Networking.java
@@ -0,0 +1,38 @@
+package org.viesure.page.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.viesure.page.common.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Networking {
+
+    static String dataURL = "https://run.mocky.io/v3/de42e6d9-2d03-40e2-a426-8953c7c94fb8";
+
+    private static Response getRequest(){
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.contentType(ContentType.JSON);
+
+        return requestSpecification.relaxedHTTPSValidation().get(dataURL);
+    }
+
+    public static List<Article> getDummyData(){
+        return parseResponse(getRequest());
+    }
+
+    private static List<Article> parseResponse(Response response){
+        List<Article> responseArticles;
+
+        Gson gson = new Gson();
+
+        responseArticles = gson.fromJson(response.then().extract().response().asString(), new TypeToken<ArrayList<Article>>(){}.getType());
+
+        return responseArticles;
+    }
+}
