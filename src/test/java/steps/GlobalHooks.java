bbfd5eb04b863821ad467c8d54a318647c2e0287
János Csizmadia <>
2021-04-06 13:52:13 +0200

    Refactored GlobalHooks to use ThreadLocal for driver paralellization

--- src/test/java/steps/GlobalHooks.java
+++ src/test/java/steps/GlobalHooks.java
@@ -1,5 +1,6 @@
 package steps;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -7,11 +8,9 @@ import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Reporter;
 import org.viesure.utils.AllureLogger;
 
 import java.io.File;
@@ -19,8 +18,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class GlobalHooks {
-    private AndroidDriver<AndroidElement> driver;
     private File app = new File(System.getProperty("user.dir") + "/app/android/" + "qa-interview.apk");
+
+    private static ThreadLocal<AndroidDriver<AndroidElement>> threadedDriver = new ThreadLocal<>();
+
 
     @Before(order = 0)
     @Step("Initalizing Android driver")
@@ -32,7 +33,7 @@ public class GlobalHooks {
     public void afterStep(Scenario scenario){
         if (scenario.isFailed()){
             AllureLogger.saveTextLog("Saved screenshot for failed step: " + scenario.getName());
-            AllureLogger.saveScreenshotPNG(driver);
+            AllureLogger.saveScreenshotPNG(threadedDriver.get());
 
         }
     }
@@ -40,27 +41,43 @@ public class GlobalHooks {
     @After(order = 1)
     @Step("Quitting android driver")
     public void tearDown(){
-        driver.quit();
+        threadedDriver.get().quit();
     }
 
     public AndroidDriver<AndroidElement> getDriver(){
-        return driver;
+        return threadedDriver.get();
     }
 
     public void initalizeAndroidDriver(){
-        DesiredCapabilities capabilities = getPixelCapabilities();
-
-        try {
-//            driver = new AndroidDriver<>(new URL("http://0.0.0.0:4728/wd/hub"), capabilities);
-            driver = new AndroidDriver<>(new URL("http://0.0.0.0:4755/wd/hub"), capabilities);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+        String device = Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter("device");
+        if (device == null){
+            device = "pixel";
         }
+        System.out.println("Device is: " + device);
+        DesiredCapabilities capabilities;
+        if (device.equals("pixel")){
+            capabilities = getPixelCapabilities();
+
+//            try {
+//                driver = new AndroidDriver<>(new URL("http://0.0.0.0:4755/wd/hub"), capabilities);
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            }
+        } else {
+            capabilities = getNexusCapabilities();
+
+//            try {
+//                driver = new AndroidDriver<>(new URL("http://0.0.0.0:4728/wd/hub"), capabilities);
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            }
+        }
+
     }
 
     @Step("Navigating back using android system's back button")
     public void navigateBack(){
-        driver.navigate().back();
+        threadedDriver.get().navigate().back();
     }
 
 
@@ -70,8 +87,15 @@ public class GlobalHooks {
         capabilities.setCapability("deviceName","Pixel XL API 30");
         capabilities.setCapability("udid","emulator-5554");
         capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("appium:chromeOptions", ImmutableMap.of("w3c", false));
         capabilities.setCapability("appPackage","io.viesure.qa");
         capabilities.setCapability("appActivity","io.viesure.qa.views.MainActivity");
+
+        try {
+            threadedDriver.set(new AndroidDriver<>(new URL("http://0.0.0.0:4755/wd/hub"), capabilities));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         return capabilities;
     }
@@ -82,9 +106,15 @@ public class GlobalHooks {
         capabilities.setCapability("deviceName","Nexus S API 24");
         capabilities.setCapability("udid","emulator-5556");
         capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("appium:chromeOptions", ImmutableMap.of("w3c", false));
         capabilities.setCapability("appPackage","io.viesure.qa");
         capabilities.setCapability("appActivity","io.viesure.qa.views.MainActivity");
 
+        try {
+            threadedDriver.set(new AndroidDriver<>(new URL("http://0.0.0.0:4728/wd/hub"), capabilities));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         return capabilities;
     }
 
